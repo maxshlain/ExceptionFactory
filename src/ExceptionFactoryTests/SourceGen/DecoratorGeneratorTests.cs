e5b1f02b478adf420b32f5769b4402e90f835986
maxim-shlain <maxshlain@gmail.com>
2022-10-28 20:56:45 +0300

    add DecoratorGeneratorTests

---
+++ src/ExceptionFactoryTests/SourceGen/DecoratorGeneratorTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+
+namespace ExceptionFactoryTests.SourceGen;
+
+public class DecoratorGeneratorTests
+{
+    [Fact]
+    public void ShouldSayHi()
+    {
+        // ARRANGE
+        var sut = new FunNs.Say();
+        
+        // ACT
+        var fromTest = sut.Hello();
+
+        // ASSERT
+        fromTest.Should().Be("Hello");
+    }
+}
